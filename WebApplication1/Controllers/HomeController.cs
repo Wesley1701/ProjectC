9ea5feb5a0f1513397f5b35956bde51f4291d788
Wesley Zwaal <1026606@hr.nl>
2022-10-04 09:53:48 +0200

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Privacy(TimeInfo T)
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            TimeInfo C = new TimeInfo();
+            C.End = "End time";
+            C.Start = "Start time";
+            return View(C);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication1/Models/TimeInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Models
+{
+    public class TimeInfo
+    {
+        public string Start { get; set; }
+        public string End { get; set; }
+    }
+}
